fe33e2b7f1b5150c4cc85e10ae370ec03a91cffe
mikoto <avdb@keemail.me>
2024-01-26 23:10:41 +0000

    feat(core/db): initialize database on startup

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -1,11 +1,14 @@
 pub mod account;
 pub mod auth;
+pub mod db;
 pub mod error;
+pub mod events;
 pub mod mail;
 pub mod util;
 
 pub use error::{Error, HttpStatusCode, Result};
 
+use events::service::EventsService;
 use mail::service::MailService;
 use url::Url;
 
@@ -23,6 +26,10 @@ pub mod env {
     pub const COMMUNE_SYNAPSE_ADMIN_TOKEN: &str = "COMMUNE_SYNAPSE_ADMIN_TOKEN";
     pub const COMMUNE_SYNAPSE_SERVER_NAME: &str = "COMMUNE_SYNAPSE_SERVER_NAME";
     pub const COMMUNE_REGISTRATION_SHARED_SECRET: &str = "COMMUNE_REGISTRATION_SHARED_SECRET";
+    pub const POSTGRES_HOST: &str = "POSTGRES_HOST";
+    pub const POSTGRES_USER: &str = "POSTGRES_USER";
+    pub const POSTGRES_PASSWORD: &str = "POSTGRES_PASSWORD";
+    pub const POSTGRES_DB: &str = "POSTGRES_DB";
     pub const REDIS_HOST: &str = "REDIS_HOST";
     pub const SMTP_HOST: &str = "SMTP_HOST";
     pub const MAILDEV_INCOMING_USER: &str = "MAILDEV_INCOMING_USER";
@@ -31,14 +38,18 @@ pub mod env {
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct CommuneConfig {
-    pub smtp_host: Url,
-    pub redis_host: Url,
-    pub maildev_incoming_user: Option<String>,
-    pub maildev_incoming_pass: Option<String>,
     pub synapse_host: String,
     pub synapse_admin_token: String,
     pub synapse_server_name: String,
     pub synapse_registration_shared_secret: String,
+    pub postgres_host: String,
+    pub postgres_user: String,
+    pub postgres_password: String,
+    pub postgres_db: String,
+    pub redis_host: Url,
+    pub smtp_host: Url,
+    pub maildev_incoming_user: Option<String>,
+    pub maildev_incoming_pass: Option<String>,
 }
 
 impl Default for CommuneConfig {
@@ -50,14 +61,18 @@ impl Default for CommuneConfig {
 impl CommuneConfig {
     pub fn new() -> Self {
         Self {
-            smtp_host: Self::var(env::SMTP_HOST),
-            redis_host: Self::var(env::REDIS_HOST),
-            maildev_incoming_user: Self::var_opt(env::MAILDEV_INCOMING_USER),
-            maildev_incoming_pass: Self::var_opt(env::MAILDEV_INCOMING_PASS),
             synapse_host: Self::var(env::COMMUNE_SYNAPSE_HOST),
             synapse_admin_token: Self::var(env::COMMUNE_SYNAPSE_ADMIN_TOKEN),
             synapse_server_name: Self::var(env::COMMUNE_SYNAPSE_SERVER_NAME),
             synapse_registration_shared_secret: Self::var(env::COMMUNE_REGISTRATION_SHARED_SECRET),
+            postgres_host: Self::var(env::POSTGRES_HOST),
+            postgres_user: Self::var(env::POSTGRES_USER),
+            postgres_password: Self::var(env::POSTGRES_PASSWORD),
+            postgres_db: Self::var(env::POSTGRES_DB),
+            redis_host: Self::var(env::REDIS_HOST),
+            smtp_host: Self::var(env::SMTP_HOST),
+            maildev_incoming_user: Self::var_opt(env::MAILDEV_INCOMING_USER),
+            maildev_incoming_pass: Self::var_opt(env::MAILDEV_INCOMING_PASS),
         }
     }
 
@@ -95,11 +110,16 @@ impl CommuneConfig {
 pub struct Commune {
     pub account: Arc<AccountService>,
     pub auth: Arc<AuthService>,
+    pub events: Arc<EventsService>,
 }
 
 impl Commune {
     pub async fn new<C: Into<CommuneConfig>>(config: C) -> Result<Self> {
         let config: CommuneConfig = config.into();
+
+        let postgres = db::connect(config.postgres_db, config.postgres_user, config.postgres_password, config.postgres_db).await?;
+        let events = EventsService::new(postgres);
+
         let mut admin = MatrixAdminClient::new(&config.synapse_host, &config.synapse_server_name)
             .map_err(|err| {
             tracing::error!(?err, "Failed to create admin client");
@@ -148,6 +168,7 @@ impl Commune {
         Ok(Self {
             account: Arc::new(account),
             auth,
+            events,
         })
     }
 }
