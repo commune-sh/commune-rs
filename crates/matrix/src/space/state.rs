71d255a1c98a032d47cf73adb2a4b10238012e41
mikoto <avdb@keemail.me>
2024-02-02 22:08:16 +0000

    fix: remove state events we might not need

--- crates/matrix/src/space/state.rs
+++ crates/matrix/src/space/state.rs
@@ -1,10 +1,6 @@
-use ruma_events::{EmptyStateKey, StateEventContent};
+use ruma_events::{EmptyStateKey};
 use ruma_macros::EventContent;
-use serde::{de::DeserializeOwned, Deserialize, Serialize};
-
-#[derive(Clone, Debug, Deserialize, Serialize, EventContent)]
-#[ruma_event(type = "space.topic", kind = State, state_key_type = EmptyStateKey)]
-pub struct SpaceTopicEventContent {}
+use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Deserialize, Serialize, EventContent)]
 #[ruma_event(type = "space.restriction", kind = State, state_key_type = EmptyStateKey)]
@@ -13,8 +9,3 @@ pub struct SpaceRestrictionEventContent {
     #[serde(flatten)]
     account_age: u32,
 }
-
-pub trait SpaceStateEventContent: StateEventContent + DeserializeOwned {}
-
-impl SpaceStateEventContent for SpaceTopicEventContent {}
-impl SpaceStateEventContent for SpaceRestrictionEventContent {}
