a3e94181c3b06471a0f71989fb28c3e6381b9a17
mikoto <avdb@keemail.me>
2024-02-11 11:17:51 +0000

    chore: add validation for `Option<String>` fields

--- crates/core/src/room/service.rs
+++ crates/core/src/room/service.rs
@@ -4,42 +4,56 @@ use tracing::instrument;
 
 use matrix::{
     client::resources::room::{
-        CreateRoomBody, JoinRoomBody, Room as MatrixRoom, RoomCreationContent, RoomPreset, ForgetRoomBody, LeaveRoomBody,
+        CreateRoomBody, ForgetRoomBody, JoinRoomBody, LeaveRoomBody, Room as MatrixRoom,
+        RoomCreationContent, RoomPreset,
     },
-    ruma_common::{OwnedUserId, OwnedRoomId, OwnedRoomOrAliasId},
+    ruma_common::{OwnedRoomId, OwnedRoomOrAliasId, OwnedUserId},
     Client as MatrixAdminClient,
 };
+use validator::Validate;
+
 use crate::{util::secret::Secret, Error, Result};
 
 use super::model::Room;
 
-#[derive(Debug, Default, Clone)]
+#[derive(Debug, Default, Validate)]
 pub struct CreateRoomDto {
+    #[validate(length(min = 1))]
     pub name: Option<String>,
+
+    #[validate(length(min = 1))]
     pub topic: Option<String>,
+
+    #[validate(length(min = 1))]
     pub alias: Option<String>,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Validate)]
 pub struct CreateDirectRoomDto {
     pub invitee: OwnedUserId,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Validate)]
 pub struct JoinRoomDto {
     pub alias_or_id: OwnedRoomOrAliasId,
+
+    #[validate(length(min = 1))]
     pub reason: Option<String>,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Validate)]
 pub struct ArchiveRoomDto {
     pub room_id: OwnedRoomId,
+
+    #[validate(length(min = 1))]
     pub reason: Option<String>,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Validate)]
 pub struct LeaveRoomDto {
     pub room_id: OwnedRoomId,
+
+    #[validate(length(min = 1))]
     pub reason: Option<String>,
 }
 
@@ -146,16 +160,14 @@ impl RoomService {
 
     /// Joins a Chat Room
     #[instrument(skip(self, dto))]
-    pub async fn join_room(
-        &self,
-        access_token: &Secret,
-        dto: JoinRoomDto,
-    ) -> Result<Room> {
+    pub async fn join_room(&self, access_token: &Secret, dto: JoinRoomDto) -> Result<Room> {
         match MatrixRoom::join(
             &self.admin,
             access_token.to_string(),
             &dto.alias_or_id,
-            JoinRoomBody { reason: dto.reason.unwrap_or_default() },
+            JoinRoomBody {
+                reason: dto.reason.unwrap_or_default(),
+            },
         )
         .await
         {
@@ -171,16 +183,14 @@ impl RoomService {
 
     /// Archives a Chat Room
     #[instrument(skip(self, dto))]
-    pub async fn archive_room(
-        &self,
-        access_token: &Secret,
-        dto: ArchiveRoomDto,
-    ) -> Result<()> {
+    pub async fn archive_room(&self, access_token: &Secret, dto: ArchiveRoomDto) -> Result<()> {
         match MatrixRoom::forget(
             &self.admin,
             access_token.to_string(),
             &dto.room_id,
-            ForgetRoomBody { reason: dto.reason.unwrap_or_default() },
+            ForgetRoomBody {
+                reason: dto.reason.unwrap_or_default(),
+            },
         )
         .await
         {
@@ -194,16 +204,14 @@ impl RoomService {
 
     /// Leaves a Chat Room
     #[instrument(skip(self, dto))]
-    pub async fn leave_room(
-        &self,
-        access_token: &Secret,
-        dto: LeaveRoomDto,
-    ) -> Result<()> {
+    pub async fn leave_room(&self, access_token: &Secret, dto: LeaveRoomDto) -> Result<()> {
         match MatrixRoom::leave(
             &self.admin,
             access_token.to_string(),
             &dto.room_id,
-            LeaveRoomBody { reason: dto.reason.unwrap_or_default() },
+            LeaveRoomBody {
+                reason: dto.reason.unwrap_or_default(),
+            },
         )
         .await
         {
