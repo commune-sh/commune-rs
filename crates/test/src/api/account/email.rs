454c638bcf9c73a4b4338cda4fa9e0f88804577b
mikoto <avdb@keemail.me>
2024-03-28 17:33:24 +0000

    wip: spaces

--- crates/test/src/api/account/email.rs
+++ crates/test/src/api/account/email.rs
@@ -1,6 +1,7 @@
 use commune::util::secret::Secret;
 use matrix::client::register::root::*;
 use router::api::register::root as register;
+use serde::Deserialize;
 use url::form_urlencoded::byte_serialize;
 
 use crate::{env::Env, util::generate_comforming_localpart};
@@ -28,6 +29,20 @@ pub async fn verify_email(client: &Env) -> Result<Response, reqwest::Error> {
 
     tracing::info!(token_hash);
 
+    let resp = reqwest::get("http://localhost:1080/api/messages").await?;
+    let emails: Vec<EmailId> = resp.json().await?;
+
+    let EmailId { id } = emails.last().unwrap();
+    let resp = reqwest::get(format!("http://localhost:1080/api/message/{id}")).await?;
+
+    let EmailBody { text } = resp.json().await?;
+    let code: String = text.chars().filter(|c| c.is_digit(10)).collect();
+
+    let engine = GeneralPurpose::new(&alphabet::URL_SAFE, general_purpose::NO_PAD);
+
+    let token_sha256 = digest::digest(&digest::SHA256, &token.as_bytes());
+    let token_sha256_b64 = engine.encode(token_sha256);
+
     let username = generate_comforming_localpart();
 
     tracing::info!(?username);
@@ -37,7 +52,7 @@ pub async fn verify_email(client: &Env) -> Result<Response, reqwest::Error> {
         .json(&register::Payload {
             username,
             password: Secret::new("verysecure"),
-            registration_token: None,
+            registration_token: Some(code),
         })
         .send()
         .await?;
@@ -45,7 +60,17 @@ pub async fn verify_email(client: &Env) -> Result<Response, reqwest::Error> {
     resp.json().await
 }
 
-// #[tokio::test]
+#[derive(Deserialize)]
+struct EmailId {
+    pub id: String,
+}
+
+#[derive(Deserialize)]
+struct EmailBody {
+    pub text: String,
+}
+
+#[tokio::test]
 async fn verify_email_test() {
     let client = Env::new().await;
 
